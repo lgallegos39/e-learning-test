9f21af8398da3acafe35b7bf64f9d57a55557b5a
Monica Mascorro <mmascorro@coley.local>
2017-07-07 14:27:58 -0500

    save

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    var $landingContent = $("#landing-content"),
+        $actualContent  = $("#actual-content");
+    
+    $landingContent.on("click", function (){
+    
+        $(this).addClass("clicked");
+    })
+ });
